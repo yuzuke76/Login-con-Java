ab583c51a9a48f25ccb074be021f16f24c7c1f1e
yuzuke76 <32428173+yuzuke76@users.noreply.github.com>
2018-08-16 22:53:35 -0500

    Update User.java

--- src/java/com/ap/logindemo/User.java
+++ src/java/com/ap/logindemo/User.java
@@ -1,48 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.ap.logindemo;
 
-/**
- *
- * @author prof.andreapollini
- */
+
 class User { 
     
-    private String username;
+    private String usuario;
     private String password;
 
-    public User(String username, String password) {
-        this.username = username;
+    public User(String usuario, String password) {
+        this.username = usuario;
         this.password = password;
     }
 
-    /**
-     * @return the username
-     */
-    public String getUsername() {
-        return username;
+   
+    public String getusuario() {
+        return usuario;
     }
 
-    /**
-     * @param username the username to set
-     */
-    public void setUsername(String username) {
-        this.username = username;
+    
+    public void setusuario(String usuario) {
+        this.usuario = usuario;
     }
 
-    /**
-     * @return the password
-     */
+   
     public String getPassword() {
         return password;
     }
 
-    /**
-     * @param password the password to set
-     */
+   
     public void setPassword(String password) {
         this.password = password;
     }
