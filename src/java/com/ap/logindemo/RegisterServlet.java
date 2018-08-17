2df5f10cde88ab002c5a8a82932a4c2ab0ae396c
yuzuke76 <32428173+yuzuke76@users.noreply.github.com>
2018-08-16 22:52:23 -0500

    Update RegisterServlet.java

--- src/java/com/ap/logindemo/RegisterServlet.java
+++ src/java/com/ap/logindemo/RegisterServlet.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.ap.logindemo;
 
 import java.io.IOException;
@@ -13,10 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author prof.andreapollini
- */
+
 @WebServlet(name = "RegisterServlet", urlPatterns = {"/register"})
 public class RegisterServlet extends HttpServlet {
 
@@ -24,7 +17,7 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        super.init(); //To change body of generated methods, choose Tools | Templates.
+        super.init(); 
 
         
         authService = new AuthenticationService();
@@ -32,12 +25,12 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username");
+        String username = req.getParameter("usuario");
         String password = req.getParameter("password");
         String passwordRep = req.getParameter("passwordRep");
         
         if(!password.equals(passwordRep)) {
-            req.setAttribute("error", "Passwords doesn't match");
+            req.setAttribute("error", "Passwords no coinciden");
             req.getRequestDispatcher("register.jsp").forward(req, resp);
             
             return;
